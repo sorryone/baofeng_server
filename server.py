cbf9caf2a5b74f4a710f7f75350a2768f1f33dbc
sorryone <jay0289@163.com>
2017-06-15 14:25:43 +0800

    server

--- server.py
+++ server.py
@@ -26,7 +26,7 @@ def set_msg_data(data, format_code, data_body=None):
     if not isinstance(data, (tuple, list)):
         raise TypeError
     buf = pickle.struct.pack(format_code, *data)
-    # print "format_code=",format_code
+    # print "format_code=", format_code
     # print "pack buf = ", len(buf)
     # print "data=", data
     buf_data = None
@@ -48,24 +48,46 @@ def client_connect(client):
     if client.factory.lobby:
         msg_data = (8, 10005, u_id)
         other_buf = set_msg_data(msg_data, "3i")
-        for c in client.factory.lobby:
-            c.transport.write(other_buf)
+        for k, v in client.factory.lobby.iteritems():
+            v.transport.write(other_buf)
 
-    client.factory.addLobby(client)
+    client.factory.addLobby(u_id, client)
 
 
 def client_login_game(client):
     print "New Player Login Game"
-    msg_data = (4, 20002)
-    buf = set_msg_data(msg_data, "2i")
+    u_id = client.factory.get_uid_by_lobby(client)
+    if client.factory.master_uid:
+        msg_data = (8, 20002, client.factory.master_uid)
+    else:
+        msg_data = (8, 20002, u_id)
+    buf = set_msg_data(msg_data, "3i")
     client.transport.write(buf)
     if client.factory.clients:
-        msg_data = (4, 20005)
-        other_buf = set_msg_data(msg_data, "2i")
-        for c in client.factory.clients:
-            c.transport.write(other_buf)
+        msg_data = (8, 20005, client.factory.master_uid)
+        other_buf = set_msg_data(msg_data, "3i")
+        for k, v in client.factory.clients.iteritems():
+            v.transport.write(other_buf)
+    else:
+        client.factory.master_uid = u_id
 
-    client.factory.addClient(client)
+    client.factory.addClient(u_id, client)
+
+
+def client_logout_game(client):
+    print "Player Logout Game"
+    u_id = client.factory.get_uid_by_lobby(client)
+    msg_data = (8, 20006, u_id)
+    buf = set_msg_data(msg_data, "3i")
+    lobby_data = (8, 10006, u_id)
+    lobby_buf = set_msg_data(lobby_data, "3i")
+    client.factory.delClient(client)
+    client.factory.delLobby(client)
+    for k, v in client.factory.clients.iteritems():
+        v.transport.write(buf)
+
+    for k, v in client.factory.lobby.iteritems():
+        v.transport.write(lobby_buf)
 
 
 class Game(LineOnlyReceiver):
@@ -88,10 +110,7 @@ class Game(LineOnlyReceiver):
         client_connect(self)
 
     def connectionLost(self, reason):
-        if self in self.factory.lobby:
-            self.factory.delLobby(self)
-        if self in self.factory.clients:
-            self.factory.delClient(self)
+        client_logout_game(self)
 
 
 class GameFactory(ServerFactory):
@@ -100,43 +119,56 @@ class GameFactory(ServerFactory):
     def __init__(self, service):
         self.service = service
         self.master_uid = None
-        self.clients = []
-        self.lobby = []
+        self.clients = {}
+        self.lobby = {}
         self.player = []
         self.msg = ''
 
     def getPlayerId(self):
         return len(self.player)
 
-    def addLobby(self, client):
-        self.lobby.append(client)
+    def addLobby(self, u_id, client):
+        self.lobby[u_id] = client
 
-    def delLobby(self, client):
-        self.lobby.remove(client)
+    def delLobby(self, u_id):
+        if u_id in self.lobby:
+            self.lobby.pop(u_id)
 
-    def addClient(self, client):
-        self.clients.append(client)
+    def get_uid_by_lobby(self, client):
+        for k, v in self.lobby.iteritems():
+            if v is client:
+                return k
 
-    def delClient(self, client):
-        self.clients.remove(client)
+    def addClient(self, u_id, client):
+        self.clients[u_id] = client
+
+    def delClient(self, u_id):
+        if u_id in self.clients:
+            self.clients.pop(u_id)
+
+    def get_uid_by_client(self, client):
+        for k, v in self.clients.iteritems():
+            if v is client:
+                return k
 
     def sendAll(self, data_head, data_body, client):
         if not isinstance(data_head, tuple):
             return
-        # print "clinets num = ", len(self.clients)
         msg_len = data_head[0]
         # msg_num = data_head[1]
         msg_code = data_head[2]
         if msg_code == 20001:
+            print "clinets num = ", len(self.clients)
+            print "recv=20001"
             client_login_game(client)
         else:
-            # print "msg_len=", msg_len
+            # print "msg_len=", msg_len, "client_id=", client.getId()
             # print "msg_num=", msg_num
-            # print "msg_code=", msg_code
+            # print "msg_code=", msg_code, "client_id=", client.getId()
             buf = set_msg_data((msg_len-4, msg_code), "2i", data_body)
-            for proto in self.clients:
-                if proto != client:
-                    proto.transport.write(buf)
+            for k, v in self.clients.iteritems():
+                if v != client:
+                    v.transport.write(buf)
 
 
 # reactor.listenTCP(9001, GameFactory())
